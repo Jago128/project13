542d58dc2df90c4267fe6e36b9a44b4de892ceda
Jago128 <jagobaetxegarai@gmail.com>
2025-02-17 13:47:05 +0100

    e

--- src/windows/PHWindow.java
+++ src/windows/PHWindow.java
@@ -1,16 +1,10 @@
 package windows;
 
 import java.awt.*;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.JButton;
-import java.awt.Color;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.awt.event.*;
+
 
 public class PHWindow extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
